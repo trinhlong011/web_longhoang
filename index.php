c15b75154d7faf69ff29b4a7d50a8de44074a391
trinh <levi02.amazingyou@gmail.com>
2021-06-04 17:00:03 +0700

    Myfood

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./Css/Style.css">
+</head>
+
+<body>
+    <header id="header">
+        <p>MY FOOD</p>
+    </header>
+    <section id="content">
+        <div>
+            <table border="0">
+                <tr>
+                    <td id="item">
+                        <img src="../img/sandwich.jpg" alt="">
+                        <h3>The Perfect Sandwich, A Real NYC Classic</h3>
+                        <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+                    </td>
+                    <td id="item">
+                        <img src="../img/steak.jpg" alt="">
+                        <h3>The Perfect Sandwich, A Real NYC Classic</h3>
+                        <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+                    </td>
+                    <td id="item">
+                        <img src="../img/cherries.jpg" alt="">
+                        <h3>The Perfect Sandwich, A Real NYC Classic</h3>
+                        <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+                    </td>
+                    <td id="item">
+                        <img src="img/wine.jpg" alt="">
+                        <h3>The Perfect Sandwich, A Real NYC Classic</h3>
+                        <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+                    </td> 
+                </tr>
+                <tr>
+                    <td id="item">
+                        <img src="img/popsicle.jpg" alt="">
+                        <h3>The Perfect Sandwich, A Real NYC Classic</h3>
+                        <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+                    </td>
+                    <td id="item">
+                        <img src="img/salmon.jpg" alt="">
+                        <h3>The Perfect Sandwich, A Real NYC Classic</h3>
+                        <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+                    </td>
+                    <td id="item">
+                        <img src="img/sandwich.jpg" alt="">
+                        <h3>The Perfect Sandwich, A Real NYC Classic</h3>
+                        <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+                    </td>
+                    <td id="item">
+                        <img src="img/croissant.jpg" alt="">
+                        <h3>The Perfect Sandwich, A Real NYC Classic</h3>
+                        <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+                    </td> 
+                </tr>
+            </table>
+
+        </div>
+    </section>
+    <footer id="footer">
+        <div id="about">
+            <h2>About Me, The Food Man</h2>
+        <img src="./img/chef.jpg" alt="">
+        <h3>I am Who I Am!</h3>
+        <h5>With Passion For Real, Good Food</h5>
+        <p>Just me, myself and I, exploring the universe of unknownment. 
+            I have a heart of love and an interest of lorem ipsum and mauris neque quam blog. 
+            I want to share my world with you. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo 
+            condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. Praesent tincidunt 
+            sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida 
+            diam non fringilla.</p>
+        </div>
+        <table>
+            <td>
+                <div id="row" class="r1">
+                    <h2>FOOTER</h2>
+                    <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta 
+                        lectus vitae, ultricies congue gravida diam non fringilla.
+                        <br>
+                        Powered by <a href="">aptech.ac.vn</a></p>
+                </div>
+            </td>
+            <td>
+                <div id="row" class="r2">
+                    <h2>BLOG POSTS</h2>
+                    <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo 
+                        condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+                </div>
+            </td>
+            <td>
+                <div id="row" class="r3">
+                    <h2>POPULAR TAGS</h2>
+                    <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, 
+                        porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+                </div>
+            </td>
+        </table>
+
+        
+
+    </footer>
+</body>
+</html>
